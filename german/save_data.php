6af26b1ab1ce1e719bdf8bacb381a5547ea684ef
hyein-jeong <46813089+hyein-jeong@users.noreply.github.com>
2020-09-15 07:39:44 +0200

    Update save_data.php

--- german/save_data.php
+++ german/save_data.php
@@ -3,8 +3,17 @@
     // the directory "data" is writable by the server (chmod 777)
     // the file path needs to be adjusted accordingly!
     // original $filename = "data/".$_POST['filename'];
-    $filename = $_POST['filename'];
-    $data = $_POST['filedata'];
+    //$filename = $_POST['filename'];
+    //$data = $_POST['filedata'];
     // write the file to disk
-    file_put_contents($filename, $data);
+    //file_put_contents($filename, $data);
+
+    function file_force_contents($dir, $contents){
+        $parts = explode('/', $dir);
+        $file = array_pop($parts);
+        $dir = '';
+        foreach($parts as $part)
+            if(!is_dir($dir .= "/$part")) mkdir($dir);
+        file_put_contents("$dir/$file", $contents);
+    }
 ?>
